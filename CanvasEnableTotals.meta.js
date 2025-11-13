4d637dfe846d1a1862c7554d1a35232284c7125c
Hum-Bao <kylericks@frontier.com>
2025-11-13 13:00:54 -0800

    Add option to exclude assignments from final calculation, change file name, and add download and update URLs

---
+++ CanvasEnableTotals.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Canvas Enable Totals
+// @namespace    humbao.dev
+// @version      1.1
+// @description  Return calculation of totals on Canvas
+// @author       HumBao
+// @include      http*://canvas.*.edu/courses/*/grades
+// @include      http*://*.instructure.com/courses/*/grades
+// @run-at document-end
+// ==/UserScript==
