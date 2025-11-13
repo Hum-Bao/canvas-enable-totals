0de82693408f6acf34aed0d72b9040206fa060fc
Hum-Bao <kylericks@frontier.com>
2025-11-13 13:14:48 -0800

    Fix version number in user.js because for some reason I cannot read

--- CanvasEnableTotals.user.js
+++ CanvasEnableTotals.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Canvas Enable Totals
 // @namespace    humbao.dev
-// @version      1.1
+// @version      1.2
 // @description  Return calculation of totals on Canvas
 // @author       HumBao
 // @include      http*://canvas.*.edu/courses/*/grades
